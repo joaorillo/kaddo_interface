71b5f07a2b2806e25ae586c337f90c465a79032f
joaorillo <joaopedronerillo@gmail.com>
2025-08-04 21:36:03 -0300

    checkpoint

--- kaddo_interface/interface/urls.py
+++ kaddo_interface/interface/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('my_profile', views.my_profile, name='my_profile'),
 ]
--- kaddo_interface/interface/views.py
+++ kaddo_interface/interface/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "interface/index.html")
+
+
+def my_profile(request):
+    return render(request, "interface/my_profile.html")
