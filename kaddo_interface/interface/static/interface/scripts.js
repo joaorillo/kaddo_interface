e837931209ec6397231012c3fffdd79e3c1d8223
joaorillo <joaopedronerillo@gmail.com>
2025-08-20 08:18:45 -0300

    click on aisles names (sidebar) working

--- kaddo_interface/interface/static/interface/scripts.js
+++ kaddo_interface/interface/static/interface/scripts.js
@@ -1,57 +1,48 @@
-let searchBarStrip;
-let searchBar;
-let searchBarInput;
-let closeSearchIcon;
-let sidebar;
-let sidebarReopenIcon;
-let categoriesColumn;
-let aislesFlagsLines;
-let aislesFlags;
-let categoryCards;
-let categoryContainer;
-let categoryCardMaionese;
+const ui = {};
 
 document.addEventListener('DOMContentLoaded', () => {
-    // User clicks on an aisle on sidebar
+    // Define 'ui' (User Interface) variables
+    ui.searchBarStrip = document.getElementById('search-bar-strip');
+    ui.searchBar = document.getElementById('search-bar');
+    ui.searchBarInput = document.getElementById('search-bar-input');
+    ui.closeSearchIcon = document.getElementById('close-search-icon');
+    ui.sidebar = document.getElementById('sidebar');
+    ui.sidebarReopenIcon = document.getElementById('sidebar-reopen-icon');
+    ui.categoriesColumn = document.getElementById('categories-column');
+    ui.aislesFlagsLines = document.getElementsByClassName('aisle-flag-line');
+    ui.aislesFlags = document.getElementsByClassName('aisle-flag');
+    ui.categoryCards = document.getElementsByClassName('category-card');
+    ui.categoryContainer = document.getElementById('category-container');
+    ui.categoryCardMaionese = document.getElementById('category-card-maionese');
+
+    // User clicks on an aisle on ui.sidebar
     const aislesNames = document.querySelectorAll('.aisle-name');
     aislesNames.forEach((aisle) => {
         var aisleName = aisle.innerHTML;
-        aisle.addEventListener('click', () => {
-            console.log(`scroll_aisle(${aisleName})`);
+        aisle.addEventListener('click', (e) => {
+            e.preventDefault();
             scroll_aisle(aisleName);
         })
     })
 
     // Search bar input clicked / Category card clicked / Close search icon clicked / Close category card clicked
-    searchBarStrip = document.getElementById('search-bar-strip');
-    searchBar = document.getElementById('search-bar');
-    searchBarInput = document.getElementById('search-bar-input');
-    closeSearchIcon = document.getElementById('close-search-icon');
-    sidebar = document.getElementById('sidebar');
-    sidebarReopenIcon = document.getElementById('sidebar-reopen-icon');
-    categoriesColumn = document.getElementById('categories-column');
-    aislesFlagsLines = document.getElementsByClassName('aisle-flag-line');
-    aislesFlags = document.getElementsByClassName('aisle-flag');
-    categoryCards = document.getElementsByClassName('category-card');
-    categoryContainer = document.getElementById('category-container');
-    categoryCardMaionese = document.getElementById('category-card-maionese');
-    searchBar.addEventListener('click', () => {
-        searchBarInput.focus();
-        if (!sidebar.classList.contains('minimized')) {
+    ui.searchBar.addEventListener('click', () => {
+        ui.searchBarInput.focus();
+        if (!ui.sidebar.classList.contains('minimized')) {
             toggleSearch();
         }
     })
-    categoryCardMaionese.addEventListener('click', () => {
-        if (!sidebar.classList.contains('minimized')) {
+    ui.categoryCardMaionese.addEventListener('click', () => {
+        if (!ui.sidebar.classList.contains('minimized')) {
             toggleSearch();
-            toggleClass(categoryContainer, 'hidden');
+            toggleClass(ui.categoryContainer, 'hidden');
         }
     })
-    closeSearchIcon.addEventListener('click', (event) => {
+    ui.closeSearchIcon.addEventListener('click', (event) => {
         event.stopPropagation(); // Prevents the click from bubbling up to #search-bar
         toggleSearch();
     })
-    sidebarReopenIcon.addEventListener('click', () => {
+    ui.sidebarReopenIcon.addEventListener('click', () => {
         toggleSearch();
     })
 })
@@ -67,6 +58,15 @@ function scroll_aisle(aisleName) {
         .find(el => el.innerHTML.trim() === aisleName);
     if (clickedAisle) {
         clickedAisle.classList.add('selected');
+        targetId = clickedAisle.getAttribute('href');
+        if (targetId.length > 1) {
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                var addedScrollingHeight = ui.categoryContainer.getBoundingClientRect().top + window.scrollY;
+                const y = targetElement.getBoundingClientRect().top + window.scrollY - addedScrollingHeight;
+                window.scrollTo({top: y, behavior: 'smooth'});
+            }
+        }
     }
 }
 
@@ -83,22 +83,22 @@ function toggleClass(element, className) {
 
 // Toggles between 'default view' and 'search view'
 function toggleSearch() {
-    searchBarInput.value = '';
-    if (sidebar.classList.contains('minimized') && !categoryContainer.classList.contains('hidden')) {
-        categoryContainer.classList.add('hidden');
+    ui.searchBarInput.value = '';
+    if (ui.sidebar.classList.contains('minimized') && !ui.categoryContainer.classList.contains('hidden')) {
+        ui.categoryContainer.classList.add('hidden');
     }
-    toggleClass(sidebar, 'minimized');
-    toggleClass(searchBarStrip, 'maximized');
-    toggleClass(categoriesColumn, 'maximized');
-    toggleClass(closeSearchIcon, 'd-none');
-    toggleClass(sidebarReopenIcon, 'd-none');
-    Array.from(categoryCards).forEach((categoryCard) => {
+    toggleClass(ui.sidebar, 'minimized');
+    toggleClass(ui.searchBarStrip, 'maximized');
+    toggleClass(ui.categoriesColumn, 'maximized');
+    toggleClass(ui.closeSearchIcon, 'd-none');
+    toggleClass(ui.sidebarReopenIcon, 'd-none');
+    Array.from(ui.categoryCards).forEach((categoryCard) => {
         toggleClass(categoryCard, 'd-none');
     })
-    Array.from(aislesFlagsLines).forEach((aisleFlagLine) => {
+    Array.from(ui.aislesFlagsLines).forEach((aisleFlagLine) => {
         toggleClass(aisleFlagLine, 'd-none');
     })
-    Array.from(aislesFlags).forEach((aisleFlag) => {
+    Array.from(ui.aislesFlags).forEach((aisleFlag) => {
         toggleClass(aisleFlag, 'd-none');
     })
 }
